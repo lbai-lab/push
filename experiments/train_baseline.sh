ace46c514539a5d522b71235ff9037e0da556b9b
dehuang <dehuang@wks-th-946-49.sfsu.edu>
2023-09-29 20:57:49 +0000

    update

--- experiments/train_baseline.sh
+++ experiments/train_baseline.sh
@@ -2,26 +2,67 @@
 
 GROUP=${1:-"DEFAULT"}
 
-python train.py -wb -g $GROUP --model unet -t ensemble -n 2 -d 1
-python train.py -wb -g $GROUP --model unet -t mswag --pretrain_epochs 3 --swag_epochs 3 -n 2 -d 1
-python train.py -wb -g $GROUP --model unet -t svgd -n 2 -d 1
+python train.py -wb -g $GROUP --model transformer -t ensemble -n 1 -d 1 -e 10 -dl 5120  || true
+python train.py -wb -g $GROUP --model transformer -t ensemble -n 2 -d 1 -e 10 -dl 5120  || true
+python train.py -wb -g $GROUP --model transformer -t ensemble -n 4 -d 1 -e 10 -dl 5120  || true
+python train.py -wb -g $GROUP --model transformer -t ensemble -n 8 -d 1 -e 10 -dl 5120  || true
+python train.py -wb -g $GROUP --model transformer -t mswag --pretrain_epochs 3 --swag_epochs 10 -n 1 -d 1 -dl 5120  || true
+python train.py -wb -g $GROUP --model transformer -t mswag --pretrain_epochs 3 --swag_epochs 10 -n 2 -d 1 -dl 5120  || true
+python train.py -wb -g $GROUP --model transformer -t mswag --pretrain_epochs 3 --swag_epochs 10 -n 4 -d 1 -dl 5120  || true
+python train.py -wb -g $GROUP --model transformer -t mswag --pretrain_epochs 3 --swag_epochs 10 -n 8 -d 1 -dl 5120  || true
+python train.py -wb -g $GROUP --model transformer -t svgd -n 1 -d 1 -e 10 -dl 5120  || true
+python train.py -wb -g $GROUP --model transformer -t svgd -n 2 -d 1 -e 10 -dl 5120  || true
+python train.py -wb -g $GROUP --model transformer -t svgd -n 4 -d 1 -e 10 -dl 5120  || true
+python train.py -wb -g $GROUP --model transformer -t svgd -n 8 -d 1 -e 10 -dl 5120  || true
 
-python train.py -wb -g $GROUP --model schnet -t ensemble -n 2 -d 1
-python train.py -wb -g $GROUP --model schnet -t mswag --pretrain_epochs 3 --swag_epochs 3 -n 2 -d 1
-python train.py -wb -g $GROUP --model schnet -t svgd -n 2 -d 1
+python train.py -wb -g $GROUP --model cgcnn -t ensemble -n 1 -d 1 -e 10 -dl 800  || true
+python train.py -wb -g $GROUP --model cgcnn -t ensemble -n 2 -d 1 -e 10 -dl 800  || true
+python train.py -wb -g $GROUP --model cgcnn -t ensemble -n 4 -d 1 -e 10 -dl 800  || true
+python train.py -wb -g $GROUP --model cgcnn -t ensemble -n 8 -d 1 -e 10 -dl 800  || true
+python train.py -wb -g $GROUP --model cgcnn -t mswag --pretrain_epochs 3 --swag_epochs 10 -n 1 -d 1 -dl 800  || true
+python train.py -wb -g $GROUP --model cgcnn -t mswag --pretrain_epochs 3 --swag_epochs 10 -n 2 -d 1 -dl 800  || true
+python train.py -wb -g $GROUP --model cgcnn -t mswag --pretrain_epochs 3 --swag_epochs 10 -n 4 -d 1 -dl 800  || true
+python train.py -wb -g $GROUP --model cgcnn -t mswag --pretrain_epochs 3 --swag_epochs 10 -n 8 -d 1 -dl 800  || true
+python train.py -wb -g $GROUP --model cgcnn -t svgd -n 1 -d 1 -e 10 -dl 800  || true
+python train.py -wb -g $GROUP --model cgcnn -t svgd -n 2 -d 1 -e 10 -dl 800  || true
+python train.py -wb -g $GROUP --model cgcnn -t svgd -n 4 -d 1 -e 10 -dl 800  || true
+python train.py -wb -g $GROUP --model cgcnn -t svgd -n 8 -d 1 -e 10 -dl 800  || true
 
-python train.py -wb -g $GROUP --model cgcnn -t ensemble -n 2 -d 1
-python train.py -wb -g $GROUP --model cgcnn -t mswag --pretrain_epochs 3 --swag_epochs 3 -n 2 -d 1
-python train.py -wb -g $GROUP --model cgcnn -t svgd -n 2 -d 1
+python train.py -wb -g $GROUP --model unet -t ensemble -n 1 -d 1 -e 10 -dl 2000  || true
+python train.py -wb -g $GROUP --model unet -t ensemble -n 2 -d 1 -e 10 -dl 2000  || true
+python train.py -wb -g $GROUP --model unet -t ensemble -n 4 -d 1 -e 10 -dl 2000  || true
+python train.py -wb -g $GROUP --model unet -t ensemble -n 8 -d 1 -e 10 -dl 2000  || true
+python train.py -wb -g $GROUP --model unet -t mswag --pretrain_epochs 3 --swag_epochs 10 -n 1 -d 1 -dl 2000  || true
+python train.py -wb -g $GROUP --model unet -t mswag --pretrain_epochs 3 --swag_epochs 10 -n 2 -d 1 -dl 2000  || true
+python train.py -wb -g $GROUP --model unet -t mswag --pretrain_epochs 3 --swag_epochs 10 -n 4 -d 1 -dl 2000  || true
+python train.py -wb -g $GROUP --model unet -t mswag --pretrain_epochs 3 --swag_epochs 10 -n 8 -d 1 -dl 2000  || true
+python train.py -wb -g $GROUP --model unet -t svgd -n 1 -d 1 -e 10 -dl 2000  || true
+python train.py -wb -g $GROUP --model unet -t svgd -n 2 -d 1 -e 10 -dl 2000  || true
+python train.py -wb -g $GROUP --model unet -t svgd -n 4 -d 1 -e 10 -dl 2000  || true
+python train.py -wb -g $GROUP --model unet -t svgd -n 8 -d 1 -e 10 -dl 2000  || true
 
-python train.py -wb -g $GROUP --model cnn -t ensemble -n 2 -d 1
-python train.py -wb -g $GROUP --model cnn -t mswag --pretrain_epochs 3 --swag_epochs 3 -n 2 -d 1
-python train.py -wb -g $GROUP --model cnn -t svgd -n 2 -d 1
+python train.py -wb -g $GROUP --model schnet -t ensemble -n 1 -d 1 -e 10 -dl 800  || true
+python train.py -wb -g $GROUP --model schnet -t ensemble -n 2 -d 1 -e 10 -dl 800  || true
+python train.py -wb -g $GROUP --model schnet -t ensemble -n 4 -d 1 -e 10 -dl 800  || true
+python train.py -wb -g $GROUP --model schnet -t ensemble -n 8 -d 1 -e 10 -dl 800  || true
+python train.py -wb -g $GROUP --model schnet -t mswag --pretrain_epochs 3 --swag_epochs 10 -n 1 -d 1 -dl 800  || true
+python train.py -wb -g $GROUP --model schnet -t mswag --pretrain_epochs 3 --swag_epochs 10 -n 2 -d 1 -dl 800  || true
+python train.py -wb -g $GROUP --model schnet -t mswag --pretrain_epochs 3 --swag_epochs 10 -n 4 -d 1 -dl 800  || true
+python train.py -wb -g $GROUP --model schnet -t mswag --pretrain_epochs 3 --swag_epochs 10 -n 8 -d 1 -dl 800  || true
+python train.py -wb -g $GROUP --model schnet -t svgd -n 1 -d 1 -e 10 -dl 800  || true
+python train.py -wb -g $GROUP --model schnet -t svgd -n 2 -d 1 -e 10 -dl 800  || true
+python train.py -wb -g $GROUP --model schnet -t svgd -n 4 -d 1 -e 10 -dl 800  || true
+python train.py -wb -g $GROUP --model schnet -t svgd -n 8 -d 1 -e 10 -dl 800  || true
 
-python train.py -wb -g $GROUP --model resnet -t ensemble -n 2 -d 1
-python train.py -wb -g $GROUP --model resnet -t mswag --pretrain_epochs 3 --swag_epochs 3 -n 2 -d 1
-python train.py -wb -g $GROUP --model resnet -t svgd -n 2 -d 1
-
-python train.py -wb -g $GROUP --model transformer -t ensemble -n 2 -d 1
-python train.py -wb -g $GROUP --model transformer -t mswag --pretrain_epochs 3 --swag_epochs 3 -n 2 -d 1
-python train.py -wb -g $GROUP --model transformer -t svgd -n 2 -d 1
+python train.py -wb -g $GROUP --model resnet -t ensemble -n 1 -d 1 -e 10 -dl 5120  || true
+python train.py -wb -g $GROUP --model resnet -t ensemble -n 2 -d 1 -e 10 -dl 5120  || true
+python train.py -wb -g $GROUP --model resnet -t ensemble -n 4 -d 1 -e 10 -dl 5120  || true
+python train.py -wb -g $GROUP --model resnet -t ensemble -n 8 -d 1 -e 10 -dl 5120  || true
+python train.py -wb -g $GROUP --model resnet -t mswag --pretrain_epochs 3 --swag_epochs 10 -n 1 -d 1 -dl 5120  || true
+python train.py -wb -g $GROUP --model resnet -t mswag --pretrain_epochs 3 --swag_epochs 10 -n 2 -d 1 -dl 5120  || true
+python train.py -wb -g $GROUP --model resnet -t mswag --pretrain_epochs 3 --swag_epochs 10 -n 4 -d 1 -dl 5120  || true
+python train.py -wb -g $GROUP --model resnet -t mswag --pretrain_epochs 3 --swag_epochs 10 -n 8 -d 1 -dl 5120  || true
+python train.py -wb -g $GROUP --model resnet -t svgd -n 1 -d 1 -e 10 -dl 5120  || true
+python train.py -wb -g $GROUP --model resnet -t svgd -n 2 -d 1 -e 10 -dl 5120  || true
+python train.py -wb -g $GROUP --model resnet -t svgd -n 4 -d 1 -e 10 -dl 5120  || true
+python train.py -wb -g $GROUP --model resnet -t svgd -n 8 -d 1 -e 10 -dl 5120  || true

22cfbe8bcda26076ad256b4a7091677f8dc65060
dehuang <dehuang@wks-th-946-49.sfsu.edu>
2023-09-29 21:40:37 +0000

    fixing path

--- experiments/quick_test.sh
+++ experiments/quick_test.sh
@@ -10,6 +10,6 @@ python train.py --model cgcnn -t svgd_push -n 1 -d 1 -e 10 -dl 800  -mef || true
 python train.py --model resnet -t ensemble_push -n 1 -d 1 -e 10 -dl 5120  || true
 python train.py --model resnet -t mswag_push -n 1 -d 1 --pretrain_epochs 3 --swag_epochs 10 -dl 5120  || true
 python train.py --model resnet -t svgd_push -n 1 -d 1 -e 10 -dl 5120  -mef || true
-python train.py --model unet -t ensemble_push -n 1 -d 1 -e 10 -dl 2000 -cp || true
-python train.py --model unet -t mswag_push -n 1 -d 1 --pretrain_epochs 3 --swag_epochs 10 -dl 2000 -cp || true
-python train.py --model unet -t svgd_push -n 1 -d 1 -e 10 -dl 2000 -cp -mef || true
+python train.py --model unet -t ensemble_push -n 1 -d 1 -e 10 -dl 2000 || true
+python train.py --model unet -t mswag_push -n 1 -d 1 --pretrain_epochs 3 --swag_epochs 10 -dl 2000 || true
+python train.py --model unet -t svgd_push -n 1 -d 1 -e 10 -dl 2000 -mef || true

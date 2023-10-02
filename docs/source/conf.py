a71b4f13e85b046e7fbe6f1bc2ba8dca1c7d3682
jontseg <jontseg@gmail.com>
2023-10-02 22:59:30 +0000

    readthedocs theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -5,6 +5,7 @@
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import sphinx_rtd_theme
 
 project = 'push'
 copyright = '2023, Daniel Huang'
@@ -14,7 +15,7 @@ release = '0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx_rtd_theme']
+extensions = []
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -24,5 +25,12 @@ language = 'python'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "sphinx_rtd_theme"
 html_static_path = ['_static']
+
+html_theme = "sphinx_rtd_theme"
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_theme_options = {
+    "collapse_navigation": False,
+    "display_version": True,
+    # 'logo_only': False,
+}

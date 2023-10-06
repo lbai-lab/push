fb7ba49297b7486bea58e77c1a8fc633bca98ce6
jontseg <jontseg@gmail.com>
2023-10-06 14:10:43 -0700

    debug

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,6 +6,8 @@
 import os
 import sys
 # sys.path.insert(0, os.path.abspath('../../'))
+print("CWD======CWD======CWD======CWD======CWD======CWD======CWD======CWD======CWD======CWD======CWD======CWD======CWD======S")
+print(os.getcwd())
 sys.path.insert(0, os.path.abspath('..'))
 sys.path.append(os.path.abspath(
     os.path.join(__file__, "../..")

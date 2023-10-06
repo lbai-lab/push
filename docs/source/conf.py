e2ce95a31af86a29a0db5b3c43bb2699b99a8d7a
jontseg <jontseg@gmail.com>
2023-10-06 13:53:36 -0700

    conf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,9 +6,10 @@
 import os
 import sys
 # sys.path.insert(0, os.path.abspath('../../'))
-sys.path.append(os.path.abspath(os.path.join(__file__, "..", "..", "..")))
-sys.path.append(os.path.abspath(os.path.join(__file__, "..", "..")))
-sys.path.insert(0, os.path.abspath('../'))
+sys.path.insert(0, os.path.abspath('..'))
+sys.path.append(os.path.abspath(
+    os.path.join(__file__, "../../push")
+))
 
 
 # Configuration file for the Sphinx documentation builder.

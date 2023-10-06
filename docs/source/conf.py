e70aeb17537e62e53c8022868f28b40663393133
jontseg <jontseg@gmail.com>
2023-10-06 14:44:59 -0700

    numpy

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -37,7 +37,7 @@ extensions = [
     'sphinx.ext.intersphinx',
     "nbsphinx"
 ]
-autodoc_mock_imports = ["tqdm"]
+autodoc_mock_imports = ["tqdm","numpy"]
 
 intersphinx_mapping = {
     'python': ('https://docs.python.org/3/', None),

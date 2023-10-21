4df2fbcf3aa65426f0b7215a65d605adba1846d2
jontseg <jontseg@gmail.com>
2023-10-21 22:59:13 +0000

    sine wave bdl tutorial

--- docs/source/tutorials/01_Bayesian_Deep_Learning/bdl.py
+++ docs/source/tutorials/01_Bayesian_Deep_Learning/bdl.py
@@ -13,7 +13,37 @@ class RandDataset(Dataset):
 
     def __getitem__(self, idx):
         return self.xs[idx], self.ys[idx]
+    
+class SineDataset(Dataset):
+    def __init__(self, batch_size, N, D):
+        self.xs = torch.linspace(0, 2 * 3.1416, batch_size * N * D).reshape(batch_size * N, D)
+        self.ys = torch.sin(self.xs[:, 0]).reshape(-1, 1)
 
+    def __len__(self):
+        return len(self.xs)
+
+    def __getitem__(self, idx):
+        return self.xs[idx], self.ys[idx]
+
+
+
+class BiggerNN(nn.Module):
+    def __init__(self, n, input_dim, output_dim, hidden_dim):
+        super(BiggerNN, self).__init__()
+        self.minis = []
+        self.n = n
+       
+        self.input_layer = nn.Linear(input_dim, hidden_dim)
+        for i in range(0, n):
+            self.minis += [MiniNN(hidden_dim)]
+            self.add_module("mini_layer"+str(i), self.minis[-1])
+        self.fc = nn.Linear(hidden_dim, output_dim)
+            
+    def forward(self, x):
+        x = self.input_layer(x)
+        for i in range(0, self.n):
+            x = self.minis[i](x)
+        return self.fc(x)
 
 class MiniNN(nn.Module):
     def __init__(self, D):

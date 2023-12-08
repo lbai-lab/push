e54d40608915e919ca34ed9de280599aa42cd33c
jontseg <jontseg@gmail.com>
2023-12-08 23:21:39 +0000

    wait docstring indentation

--- push/lib/waitable.py
+++ push/lib/waitable.py
@@ -2,13 +2,14 @@ from typing import *
 
 
 class Waitable:
-    """An abstract class representing an object with a waitable operation.
+    """
+    An abstract class representing an object with a waitable operation.
 
     Attributes:
         None
 
     Methods:
-        _wait(fid: int) -> Any: 
+        _wait(fid) -> Any: 
             Wait for the completion of a specific operation identified by the future ID (fid).
             This method should be implemented by subclasses.
 

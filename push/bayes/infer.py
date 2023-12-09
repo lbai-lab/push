6f28971c66617d0e00df4327a8ee6d17531a988e
Daniel Huang <dan.e.huang@gmail.com>
2023-12-09 20:17:19 +0000

    fixing infer interface

--- push/bayes/infer.py
+++ push/bayes/infer.py
@@ -34,7 +34,7 @@ class Infer:
     def bayes_infer(self, dataloader: DataLoader, epochs: int, **kwargs) -> None:
         """Bayesian inference method.
 
-        This method should be overridden by a child class.
+        This method should be overridden by subclass.
 
         Args:
             dataloader (DataLoader): The dataloader to use for training.
@@ -45,6 +45,21 @@ class Infer:
         """
         raise NotImplementedError
     
+    def posterior_pred(self, data: DataLoader, f_reg=True, mode="mean") -> torch.Tensor:
+        """Posterior prediction.
+
+        This method should be overridden by subclass.
+
+        Args:
+            data (DataLoader): Test data.
+            f_reg (bool, optional): Set to True for regression task. Defaults to True.
+            mode (str, optional): Type of posterior prediction to use. Defaults to "mean".
+
+        Returns:
+            torch.Tensor: Tensor of predictions
+        """
+        raise NotImplementedError
+
     def p_parameters(self) -> List[List[torch.Tensor]]:
         """Return parameters of all particles.
         
@@ -53,86 +68,86 @@ class Infer:
         """
         return [self.push_dist.p_parameters(pid) for pid in self.push_dist.particle_ids()]
 
-    def get_models(self):
-        """Instantiates models with particle parameters.
+    # def get_models(self):
+    #     """Instantiates models with particle parameters.
 
-        Returns:
-            List[torch.nn.Module]: List of instantiated models with particle parameters.
+    #     Returns:
+    #         List[torch.nn.Module]: List of instantiated models with particle parameters.
 
-        Raises:
-            AssertionError: If the size of the parameters for a model does not match the size of the model's parameters.
-        """ 
-        models = []
-        p_parameters = self.p_parameters()
-        for i in range(len(p_parameters)):
-            model = self.mk_nn(*self.args)
-            assert len(p_parameters[i]) == len(list(model.parameters())), "Model {i}'s parameter size must equal particle {i}'s"
-            for model_param, param_value in zip(model.parameters(), p_parameters[i]):
-                model_param.data = param_value
-            models.append(model)
-        return models
+    #     Raises:
+    #         AssertionError: If the size of the parameters for a model does not match the size of the model's parameters.
+    #     """ 
+    #     models = []
+    #     p_parameters = self.p_parameters()
+    #     for i in range(len(p_parameters)):
+    #         model = self.mk_nn(*self.args)
+    #         assert len(p_parameters[i]) == len(list(model.parameters())), "Model {i}'s parameter size must equal particle {i}'s"
+    #         for model_param, param_value in zip(model.parameters(), p_parameters[i]):
+    #             model_param.data = param_value
+    #         models.append(model)
+    #     return models
 
-    def get_output(self, dataloader: DataLoader) -> List[List[torch.Tensor]]:
-        """Calculates model predictions on a given dataloader.
+    # def get_output(self, dataloader: DataLoader) -> List[List[torch.Tensor]]:
+    #     """Calculates model predictions on a given dataloader.
 
-        Args:
-            dataloader (DataLoader): DataLoader containing the input data.
+    #     Args:
+    #         dataloader (DataLoader): DataLoader containing the input data.
 
-        Returns:
-            List[List[torch.Tensor]]: List of model predictions for each batch.
+    #     Returns:
+    #         List[List[torch.Tensor]]: List of model predictions for each batch.
 
-        """
-        # Instanstiate models
-        models = self.get_models()
-        outputs = []
+    #     """
+    #     # Instanstiate models
+    #     models = self.get_models()
+    #     outputs = []
     
-        for i in range(len(models)):
-            model_i_output = []
-            # Calculate output for each model
-            for data, label in dataloader:
-                model_i_output.extend(models[i](data).detach())
-            outputs.append(model_i_output)
-        return outputs
+    #     for i in range(len(models)):
+    #         model_i_output = []
+    #         # Calculate output for each model
+    #         for data, label in dataloader:
+    #             model_i_output.extend(models[i](data).detach())
+    #         outputs.append(model_i_output)
+    #     return outputs
 
-    def get_avg_pred(self, outputs: List[List[torch.Tensor]]) -> List[List[torch.Tensor]]:
-        """Calculates the average predictions over different models.
+    # def get_avg_pred(self, outputs: List[List[torch.Tensor]]) -> List[List[torch.Tensor]]:
+    #     """Calculates the average predictions over different models.
 
-        Args:
-            outputs (List[List[torch.Tensor]]): List of model predictions for each batch.
+    #     Args:
+    #         outputs (List[List[torch.Tensor]]): List of model predictions for each batch.
 
-        Returns:
-            List[torch.Tensor]: List of tensors representing the average predictions over different models.
+    #     Returns:
+    #         List[torch.Tensor]: List of tensors representing the average predictions over different models.
 
-        """
-        transposed_lists = list(map(list, zip(*outputs)))
-        averages = []
+    #     """
+    #     transposed_lists = list(map(list, zip(*outputs)))
+    #     averages = []
 
-        # Calculate the average predictions over each model
-        for tensors in transposed_lists:
-            stacked_tensor = torch.stack(tensors)
-            average_tensor = torch.mean(stacked_tensor, dim=0)
-            averages.append(average_tensor)
-        return averages
+    #     # Calculate the average predictions over each model
+    #     for tensors in transposed_lists:
+    #         stacked_tensor = torch.stack(tensors)
+    #         average_tensor = torch.mean(stacked_tensor, dim=0)
+    #         averages.append(average_tensor)
+    #     return averages
 
-    def get_median_pred(self, outputs: List[List[torch.Tensor]]) -> List[List[torch.Tensor]]:
-        """Calculates the median predictions over different models.
+    # def get_median_pred(self, outputs: List[List[torch.Tensor]]) -> List[List[torch.Tensor]]:
+    #     """Calculates the median predictions over different models.
 
-        Args:
-            outputs (List[List[torch.Tensor]]): List of model predictions for each batch.
+    #     Args:
+    #         outputs (List[List[torch.Tensor]]): List of model predictions for each batch.
 
-        Returns:
-            List[torch.Tensor]: List of tensors representing the median predictions over different models.
+    #     Returns:
+    #         List[torch.Tensor]: List of tensors representing the median predictions over different models.
 
-        """
-        transposed_lists = list(map(list, zip(*outputs)))
-        medians = []
+    #     """
+    #     transposed_lists = list(map(list, zip(*outputs)))
+    #     medians = []
 
-        # Calculate the median predictions over each model
-        for tensors in transposed_lists:
-            stacked_tensor = torch.stack(tensors)
-            median_tensor = torch.median(stacked_tensor, dim=0).values
-            medians.append(median_tensor)
-        return medians
+    #     # Calculate the median predictions over each model
+    #     for tensors in transposed_lists:
+    #         stacked_tensor = torch.stack(tensors)
+    #         median_tensor = torch.median(stacked_tensor, dim=0).values
+    #         medians.append(median_tensor)
+    #     return medians
 
     def get_var(self, outputs: List[List[torch.Tensor]]) -> List[List[torch.Tensor]]:
         """Calculates the variance of predictions over different models.
@@ -162,4 +177,3 @@ class Infer:
 
     def __exit__(self, exc_type, exc_value, traceback):
         self.push_dist.__exit__(exc_type, exc_value, traceback)
-        

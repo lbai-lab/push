5d567424ae5ada74c9061279f805a206550d484e
jontseg <jontseg@gmail.com>
2024-01-22 19:45:19 +0000

    added lenet to experiments models

--- experiments/nns/lenet/lenet.py
+++ experiments/nns/lenet/lenet.py
@@ -3,37 +3,31 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class LeNet(nn.Module):
+    """
+    "Gradient-based learning applied to document recognition" <https://ieeexplore.ieee.org/document/726791> 
+    """
     def __init__(self):
-        super(LeNet, self).__init__()
-
-        # 1st layer
-        self.conv1 = nn.Conv2d(1, 6, kernel_size=5, padding=2, stride=1)
-        # 2nd layer
-        self.conv2 = nn.Conv2d(6, 16, kernel_size=5, padding=2, stride=1)
-        # 3rd layer
-        self.conv3 = nn.Conv2d(16, 120, kernel_size=5, padding=2, stride=1)  # Adjusted kernel size and stride
-
-        # Fully connected layers
-        # Adjust the input size of fc1 to match the output size of conv3
-        self.fc1 = nn.Linear(120, 84)
-        self.fc2 = nn.Linear(84, 10)
-
+        super().__init__()
+        self.feature = nn.Sequential(
+            #1
+            nn.Conv2d(in_channels=1, out_channels=6, kernel_size=5, stride=1, padding=2),   # 28*28->32*32-->28*28
+            nn.Tanh(),
+            nn.AvgPool2d(kernel_size=2, stride=2),  # 14*14
+            
+            #2
+            nn.Conv2d(in_channels=6, out_channels=16, kernel_size=5, stride=1),  # 10*10
+            nn.Tanh(),
+            nn.AvgPool2d(kernel_size=2, stride=2),  # 5*5
+            
+        )
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(in_features=16*5*5, out_features=120),
+            nn.Tanh(),
+            nn.Linear(in_features=120, out_features=84),
+            nn.Tanh(),
+            nn.Linear(in_features=84, out_features=10),
+        )
+        
     def forward(self, x):
-        # 1st layer
-        x = F.relu(F.avg_pool2d(self.conv1(x), 2))
-
-        # 2nd layer
-        x = F.relu(F.avg_pool2d(self.conv2(x), 2))
-
-        # 3rd layer
-        x = F.relu(self.conv3(x))
-
-        x = x.view(x.size(0), -1)  # flatten
-
-        # 4th layer
-        x = F.relu(self.fc1(x))
-
-        # 5th layer
-        x = self.fc2(x)
-
-        return x
+        return self.classifier(self.feature(x))

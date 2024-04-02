ca4f6da92f3143a62d192889ca47c6cd3612d896
jontseg <jontseg@gmail.com>
2024-04-02 22:21:10 +0000

    swag and svgd logit and probability plots

--- experiments/nns/bdl.py
+++ experiments/nns/bdl.py
@@ -105,6 +105,17 @@ class SelectMNISTDataset(Dataset):
 
         return image, label
 
+class AddGaussianNoise(object):
+    def __init__(self, mean=0., std=1.):
+        self.std = std
+        self.mean = mean
+        
+    def __call__(self, tensor):
+        return tensor + torch.randn(tensor.size()) * self.std + self.mean
+    
+    def __repr__(self):
+        return self.__class__.__name__ + '(mean={0}, std={1})'.format(self.mean, self.std)
+
 # =============================================================================
 # Architecture
 # =============================================================================
